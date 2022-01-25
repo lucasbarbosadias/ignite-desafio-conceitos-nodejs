496789d038d41e5d9679c48364f232ad84005c08
Lucas Barbosa Dias <lucasbarbosadias@outlook.com>
2022-01-24 22:52:26 -0300

    fix: status code

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   todo.title = title;
   todo.deadline = new Date(deadline);
 
-  return response.status(201).json(todo);
+  return response.status(200).json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
